817412e00fbe5c152a4d65cb6c88d30dd872be9f
Rahat Ahmed <rahatarmanahmed@gmail.com>
2016-06-09 19:37:41 -0500

    Initial implementation of package manager

--- app/scripts/core/packageManager.jsx
+++ app/scripts/core/packageManager.jsx
@@ -1,12 +1,70 @@
+import _ from 'lodash';
+
 import Squelch from './squelchGlobal';
 
 export default class PackageManager {
     constructor() {
-        // TODO: don't hardcode the core command package
-        this.packages = {
-            core: require('../commands/coreCommandPackage')(Squelch)
-        };
+        this.packages = {};
+
+        // Core package is preloaded
+        this.loadPackage('core', require('../commands/coreCommandPackage'));
     }
 
-    // TODO: implement adding/removing/toggling/retrieving packages
+    /**
+     * Loads and enables a package.
+     * @todo Document how packages work
+     * @param  {[type]} name The name of the package to register it under. Must be unique.
+     * @param  {[type]} pkg  The package itself. Must either be an object or a function that returns an object.
+     */
+    loadPackage(name, pkg) {
+        if(this.packages[name]) {
+            throw new Error(`Package name conflict: a package has already been registered under ${name}`);
+        }
+
+        // Invoke function to get package object
+        if(_.isFunction(pkg)) pkg = pkg(Squelch);
+
+        this.pkgs[name] = pkg;
+        this.enablePackage(name);
+    }
+
+    /**
+     * Enables a package.
+     * @param  {string} name The name that the package was registered under.
+     */
+    enablePackage(name) {
+        const pkg = this.packages[name];
+
+        if(!pkg) throw new Error(`Package ${name} does not exist.`);
+
+        // Don't reenable
+        if(pkg.enabled) return;
+
+        pkg.enabled = true;
+        pkg.initialize(Squelch);
+    }
+
+    /**
+     * Disables a package.
+     * @param  {string} name The name that the package was registered under.
+     */
+    disablePackage(name) {
+        const pkg = this.packages[name];
+
+        if(!pkg) throw new Error(`Package ${name} does not exist.`);
+
+        // Don't redisable
+        if(!pkg.enabled) return;
+
+        pkg.enabled = false;
+        pkg.destroy(Squelch);
+    }
+
+    /**
+     * Gets a package.
+     * @param  {string} name The name that the package was registered under.
+     */
+    getPackage(name) {
+        return this.packages[name];
+    }
 }
